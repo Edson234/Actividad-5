97ef6bf9bef1e65f5dbb0705a594060524a06753
Edson Coj <Edson Coj@DESKTOP-CBPJCOK>
2021-05-06 01:58:18 -0500

    Actividad 3

--- src/actividad/pkg5/Tarea4.java
+++ src/actividad/pkg5/Tarea4.java
@@ -96,6 +96,8 @@ public class Tarea4 {
     Preguntas[9][1] = "0";  
     
     
+    
+    
             }
             
         }
--- src/actividad/pkg5/Tarea6.java
+++ src/actividad/pkg5/Tarea6.java
@@ -9,15 +9,13 @@ package actividad.pkg5;
  *
  * @author Edson Coj
  */
-public class Tarea5 {
+public class Tarea6 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        int x=1;
     }
     
 }
